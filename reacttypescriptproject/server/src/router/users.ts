f6b88f142b753c73aad0af4ff13f5af6a449fa0f
Sk Rafikul Islam <92181518+skrafikulislam@users.noreply.github.com>
2024-07-11 12:10:54 +0530

    Delete reacttypescriptproject directory

--- reacttypescriptproject/server/src/router/users.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-import { getAllUsers } from "../controllers/users.ts";
-import { isAuthenticated } from "../middlewares/index.ts";
-
-export default (router: express.Router) => {
-  router.get("/users", isAuthenticated, getAllUsers);
-};
